2db3e660074d2c425f336e8c099d48dc998af6ea
Jere Käpyaho <2261936+jerekapyaho@users.noreply.github.com>
2021-12-09 19:41:34 +0200

    Added helpers and tests

--- Sources/SyxPack/SyxPack.swift
+++ Sources/SyxPack/SyxPack.swift
@@ -1,6 +1,140 @@
-public struct SyxPack {
-    public private(set) var text = "Hello, World!"
+import Foundation
 
-    public init() {
+typealias Byte = UInt8
+typealias ByteArray = [Byte]
+
+extension Data {
+    var bytes: ByteArray {
+        var byteArray = ByteArray(repeating: 0, count: self.count)
+        self.copyBytes(to: &byteArray, count: self.count)
+        return byteArray
+    }
+}
+
+struct HexDumpConfig {
+    struct IncludeOptions: OptionSet {
+        let rawValue: UInt8
+
+        static let offset = IncludeOptions(rawValue: 1)
+        static let printableCharacters = IncludeOptions(rawValue: 1 << 1)
+        static let midChunkGap = IncludeOptions(rawValue: 1 << 2)
+    }
+    
+    var bytesPerLine: Int
+    var uppercased: Bool
+    var includeOptions: IncludeOptions
+    var indent: Int
+    
+    static let defaultConfig = HexDumpConfig(
+        bytesPerLine: 16,
+        uppercased: true,
+        includeOptions: [.offset, .printableCharacters, .midChunkGap],
+        indent: 0
+    )
+}
+
+struct SourceDumpConfig {
+    var bytesPerLine: Int
+    var uppercased: Bool
+    var variableName: String
+    var typeName: String
+    var indent: Int
+    
+    static let defaultConfig = SourceDumpConfig(
+        bytesPerLine: 16,
+        uppercased: true,
+        variableName: "data",
+        typeName: "[UInt8]",
+        indent: 4
+    )
+}
+
+extension Array {
+    func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
+extension Character {
+    var isPrintable: Bool {
+        if let v = self.asciiValue {
+            if v >= 0x20 && v < 0x7f {
+                return true
+            }
+        }
+        return false
+    }
+}
+
+extension ByteArray {
+    func hexDump(config: HexDumpConfig = .defaultConfig) -> String {
+        var lines = [String]()
+
+        let chunks = self.chunked(into: config.bytesPerLine)
+        var offset = 0
+        let hexModifier = config.uppercased ? "X" : "x"
+        let midChunkIndex = config.bytesPerLine / 2
+        for chunk in chunks {
+            var line = String(repeating: " ", count: config.indent)
+            var printableCharacters = ""
+
+            if config.includeOptions.contains(.offset) {
+                line += String(format: "%08\(hexModifier)", offset)
+                line += ": "
+            }
+        
+            for (index, byte) in chunk.enumerated() {
+                line += String(format: "%02\(hexModifier)", byte)
+                line += " "
+                
+                if index + 1 == midChunkIndex {
+                    if config.includeOptions.contains(.midChunkGap) {
+                        line += " "
+                    }
+                }
+                
+                let ch = Character(Unicode.Scalar(byte))
+                printableCharacters += ch.isPrintable ? String(ch) : "."
+            }
+
+            // Insert spaces for each unused byte slot in the chunk
+            var bytesLeft = config.bytesPerLine - chunk.count
+            while bytesLeft >= 0 {
+                line += "   "  // this is for the byte, to replace "XX "
+                printableCharacters += " "  // handle characters too, even if we don't use them
+                bytesLeft -= 1
+            }
+
+            if config.includeOptions.contains(.printableCharacters) {
+                line += " "
+                line += printableCharacters
+            }
+            
+            lines.append(line)
+            offset += config.bytesPerLine
+        }
+        
+        return lines.joined(separator: "\n")
+    }
+
+    func sourceDump(config: SourceDumpConfig = .defaultConfig) -> String {
+        var lines = [String]()
+        
+        lines.append("let \(config.variableName): \(config.typeName) = [")
+
+        let chunks = self.chunked(into: config.bytesPerLine)
+        let hexModifier = config.uppercased ? "X" : "x"
+        for chunk in chunks {
+            var line = String(repeating: " ", count: config.indent)
+            for byte in chunk {
+                line.append("0x\(String(format: "%02\(hexModifier)", byte)), ")
+            }
+            lines.append(line)
+        }
+
+        lines.append("]")
+        return lines.joined(separator: "\n")
     }
 }
--- Tests/SyxPackTests/SyxPackTests.swift
+++ Tests/SyxPackTests/SyxPackTests.swift
@@ -2,10 +2,29 @@ import XCTest
 @testable import SyxPack
 
 final class SyxPackTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SyxPack().text, "Hello, World!")
+    func test_chunked_arrayLengthIsLessThanChunkSize() {
+        let data: ByteArray = [0x41, 0x42, 0x43]
+        let chunks = data.chunked(into: 16)
+        XCTAssertEqual(chunks.count, 1)
+    }
+
+    func test_chunked_arrayLengthIsExactlyChunkSize() {
+        let data = ByteArray(repeating: 0x42, count: 16)
+        let chunks = data.chunked(into: 16)
+        XCTAssertEqual(chunks.count, 1)
+    }
+
+    func test_chunked_arrayLengthIsGreaterThanChunkSize() {
+        let data = ByteArray(repeating: 0x42, count: 24)
+        let chunks = data.chunked(into: 16)
+        XCTAssertEqual(chunks.count, 2)
+        XCTAssertEqual(chunks.last!.count, 8)
+    }
+
+    func test_sourceCodeIsGeneratedCorrectly() {
+        let data: ByteArray = [0x41, 0x42, 0x43]
+        let config = SourceDumpConfig(bytesPerLine: 8, uppercased: true, variableName: "data", typeName: "ByteArray", indent: 4)
+        let dump = data.sourceDump(config: config)
+        XCTAssertEqual(dump, "let data: ByteArray = [\n    0x41, 0x42, 0x43, \n]")
     }
 }
